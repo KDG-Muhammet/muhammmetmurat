11dc4e0f5c77043af00592989e7dbce0346ed10a
muhammet murat <muhammet.murat@student.kdg.be>
2025-09-26 19:01:17 +0200

    header componenet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './component/header/header';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, Header, Home],
+  template: `
+    <app-header />
+    <main>
+      <app-home />
+    </main>
+  `,
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('muhammetmurat');
